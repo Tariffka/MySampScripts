1bf5e32e3d35532b4ec7045c189c104c6e7faeed
Tariffka <tariff.uk@gmail.com>
2026-06-04 22:55:19 +0300

    Update [TJ] rib_Glaz.lua

--- [TJ] rib_Glaz.lua
+++ [TJ] rib_Glaz.lua
@@ -1,6 +1,11 @@
 script_name('Рыбий глаз')
-script_version_number(001)
+script_version('1.0')
 script_author('tarif_jan')
+
+-- Подключаем кодировку
+local encoding = require("encoding")
+encoding.default = 'CP1251'
+local u8 = encoding.UTF8
 
 local enabled = true
 local locked = false
@@ -13,9 +18,9 @@ function main()
 	sampRegisterChatCommand('glaz', function()
 		enabled = not enabled
 		if enabled then
-			msg('Эффект {00FF00}включен')
+			msg(u8:decode('Эффект {00FF00}включен'))
 		else
-			msg('Эффект {FF0000}выключен')
+			msg(u8:decode('Эффект {FF0000}выключен'))
 			-- Возвращаем стандартный FOV при выключении, чтобы камера не застревала
 			cameraSetLerpFov(70.0, 70.0, 1000, 1) 
 		end
@@ -26,15 +31,15 @@ function main()
 		-- Проверяем, что ввели число и оно в адекватных пределах (от 10 до 150)
 		if fovValue and fovValue >= 10 and fovValue <= 150 then
 			currentFov = fovValue
-			msg('Значение FOV успешно изменено на: {CD5C5C}' .. currentFov)
+			msg(u8:decode('Значение FOV успешно изменено на: {CD5C5C}') .. currentFov)
 		else
-			msg('Ошибка! Используйте: {CD5C5C}/fov [10 - 150]')
+			msg(u8:decode('Ошибка! Используйте: {CD5C5C}/fov [10 - 150]'))
 		end
 	end)
 	--[[_______________COMMANDS_______________]]--
 	
-	msg(string.format('Скрипт подгружен. Владелец: {CD5C5C}[%s]', thisScript().authors[1]))
-	msg('Команды: {CD5C5C}/glaz {ffffff}— вкл/выкл, {CD5C5C}/fov [10-150] {ffffff}— угол обзора')
+	msg(u8:decode(string.format('Скрипт подгружен. Владелец: {CD5C5C}[%s]', thisScript().authors[1])))
+	msg(u8:decode('Команды: {CD5C5C}/glaz {ffffff}— вкл/выкл, {CD5C5C}/fov [10-150] {ffffff}— угол обзора'))
 	
 	while true do
 		wait(0)
@@ -54,5 +59,6 @@ function main()
 end
 
 function msg(text)
-	sampAddChatMessage(string.format('[%s] {ffffff}%s', thisScript().name, text), 0xFFCD5C5C)
+	-- Оборачиваем имя скрипта и текст в u8:decode для корректного вывода
+	sampAddChatMessage(u8:decode(string.format('[%s] {ffffff}%s', thisScript().name, text)), 0xFFCD5C5C)
 end
