1b37ec17e7912bbbf5f802923372c168638d9f87
Tariffka <tariff.uk@gmail.com>
2026-06-04 22:55:48 +0300

    Update [TJ] trmap.lua

--- [TJ] trmap.lua
+++ [TJ] trmap.lua
@@ -3,37 +3,40 @@ script_name('Карта Кладов')
 script_version('1.0')
 
 local sampev = require 'lib.samp.events'
+local encoding = require 'encoding'
+encoding.default = 'CP1251'
+local u8 = encoding.UTF8
 
-local sName = '{368b55}[Карта]{FFFFFF} – '
+local sName = u8:decode('{368b55}[Карта]{FFFFFF} – ')
 local zoneActive = false
 local mapUsed = false
 local kdcond = false
-local endkd = 'Неизвестно.'
+local endkd = u8:decode('Неизвестно.')
 local autoCopyEnabled = true -- Флаг для автоматического копирования
 
 function main()
     while not isSampAvailable() do wait(0) end
     wait(100)
-    sampAddChatMessage(sName .. 'Используйте команды: /zonecopy, /zonepaste, /zonedel, /iskd, /autocopy', -1)
+    sampAddChatMessage(sName .. u8:decode('Используйте команды: /zonecopy, /zonepaste, /zonedel, /iskd, /autocopy'), -1)
     
     -- Команда для включения/выключения авто-копирования
     sampRegisterChatCommand('autocopy', function()
         autoCopyEnabled = not autoCopyEnabled
-        sampAddChatMessage(sName .. (autoCopyEnabled and 'Авто-копирование включено!' or 'Авто-копирование выключено!'), -1)
+        sampAddChatMessage(sName .. (autoCopyEnabled and u8:decode('Авто-копирование включено!') or u8:decode('Авто-копирование выключено!')), -1)
     end)
     
     sampRegisterChatCommand('zonedel', function()
         removeGangZone(610)
-        sampAddChatMessage(sName .. 'Прошлая территория была удалена!', -1)
+        sampAddChatMessage(sName .. u8:decode('Прошлая территория была удалена!'), -1)
     end)
     
     sampRegisterChatCommand('zonecopy', function()
         if mapUsed then
             setClipboardText('/zonepaste l: ' .. left .. '; u: ' .. up .. '; r: ' .. right .. '; d: ' .. down)
-            print('/zonepaste l: ' .. left .. '; u: ' .. up .. '; r: ' .. right .. '; d: ' .. down .. ' - скопировано')
-            sampAddChatMessage(sName .. 'Скопировано! Отправь это человеку, с которым хочешь поделиться координатами.', -1)
+            print('/zonepaste l: ' .. left .. '; u: ' .. up .. '; r: ' .. right .. '; d: ' .. down .. u8:decode(' - скопировано'))
+            sampAddChatMessage(sName .. u8:decode('Скопировано! Отправь это человеку, с которым хочешь поделиться координатами.'), -1)
         else
-            sampAddChatMessage(sName .. 'Активируй карту кладов, чтобы скопировать координаты.', -1)
+            sampAddChatMessage(sName .. u8:decode('Активируй карту кладов, чтобы скопировать координаты.'), -1)
         end
     end)
     
@@ -46,29 +49,29 @@ function main()
                     removeGangZone(610)
                     addGangZone(610, pLeft, pUp, pRight, pDown, -2130706433)
                     zoneActive = true
-                    sampAddChatMessage(sName .. 'Территория была успешно добавлена на твою карту!', -1)
+                    sampAddChatMessage(sName .. u8:decode('Территория была успешно добавлена на твою карту!'), -1)
                 else
-                    sampAddChatMessage(sName .. 'Не тот формат! Вставь сюда координаты, которые тебе отправил друг с картой кладов.', -1)
+                    sampAddChatMessage(sName .. u8:decode('Не тот формат! Вставь сюда координаты, которые тебе отправил друг с картой кладов.'), -1)
                 end
             else
-                sampAddChatMessage(sName .. 'Ты ничего не ввёл! Вставь сюда координаты, которые тебе отправил друг с картой кладов.', -1)
+                sampAddChatMessage(sName .. u8:decode('Ты ничего не ввёл! Вставь сюда координаты, которые тебе отправил друг с картой кладов.'), -1)
             end
         else
             if zoneActive then
                 removeGangZone(610)
                 zoneActive = false
-                sampAddChatMessage(sName .. 'Территория была удалена. Чтобы вернуть, введи координаты еще раз!', -1)
+                sampAddChatMessage(sName .. u8:decode('Территория была удалена. Чтобы вернуть, введи координаты еще раз!'), -1)
             else
-                sampAddChatMessage(sName .. 'Не тот формат! Вставь сюда координаты, которые тебе отправил друг с картой кладов.', -1)
+                sampAddChatMessage(sName .. u8:decode('Не тот формат! Вставь сюда координаты, которые тебе отправил друг с картой кладов.'), -1)
             end
         end
     end)
     
     sampRegisterChatCommand('iskd', function(arg)
         if kdcond == true then
-            sampAddChatMessage('{FF0000}[Карта]{FFFFFF} – У тебя кулдаун. Ты не можешь использовать карту! Твой кулдаун кончится ' .. endkd, -1)
+            sampAddChatMessage(u8:decode('{FF0000}[Карта]{FFFFFF} – У тебя кулдаун. Ты не можешь использовать карту! Твой кулдаун кончится ') .. endkd, -1)
         elseif kdcond == false then
-            sampAddChatMessage('{3cb043}[Карта]{FFFFFF} – Кулдауна нет. Можно использовать карту!', -1)
+            sampAddChatMessage(u8:decode('{3cb043}[Карта]{FFFFFF} – Кулдауна нет. Можно использовать карту!'), -1)
         end
     end)
     
@@ -76,26 +79,24 @@ function main()
         wait(0)
         local timenow = os.date('%X')
         if timenow == endkd then
-            printStyledString('~r~COOLDOWN END', 5000, 6)
-            sampAddChatMessage('{3cb043}[Карта]{FFFFFF} – Кулдаун прошел! Карту можно использовать!', -1)
+            printStyledString(u8:decode('~r~COOLDOWN END'), 5000, 6)
+            sampAddChatMessage(u8:decode('{3cb043}[Карта]{FFFFFF} – Кулдаун прошел! Карту можно использовать!'), -1)
             kdcond = false
-            endkd = 'Неизвестно.'
+            endkd = u8:decode('Неизвестно.')
         end
     end
 end
 
-
 function sampev.onServerMessage(_, text)
-    if autoCopyEnabled and text:find('Территория найдена! После ее исчезновения она будет автоматически восстановлена') then
+    if autoCopyEnabled and text:find(u8:decode('Территория найдена! После ее исчезновения она будет автоматически восстановлена')) then
         lua_thread.create(function()
             wait(200)
-			sampAddChatMessage(sName .. 'Текст был обнаружен, производится автоматический /zonecopy', -1)
-			wait(400)
-			sampProcessChatInput("/zonecopy")
+            sampAddChatMessage(sName .. u8:decode('Текст был обнаружен, производится автоматический /zonecopy'), -1)
+            wait(400)
+            sampProcessChatInput("/zonecopy")
         end)
     end
 end
-
 
 function sampev.onCreateGangZone(zoneId, squareStart, squareEnd, color)
     if color == -16776961 then
@@ -105,9 +106,8 @@ function sampev.onCreateGangZone(zoneId, squareStart, squareEnd, color)
         up = squareStart.y
         right = squareEnd.x
         down = squareEnd.y
-        print('l: ' .. left .. '; u: ' .. up .. '; r: ' .. right .. '; d: ' .. down)
-        sampAddChatMessage(sName .. 'Территория найдена! После ее исчезновения она будет автоматически восстановлена.', -1)
-        sampAddChatMessage(sName .. 'Чтобы скопировать ее координаты, пропиши /zonecopy', -1)
+        sampAddChatMessage(sName .. u8:decode('Территория найдена! После ее исчезновения она будет автоматически восстановлена.'), -1)
+        sampAddChatMessage(sName .. u8:decode('Чтобы скопировать ее координаты, пропиши /zonecopy'), -1)
     end
 end
 
@@ -116,14 +116,14 @@ function sampev.onGangZoneDestroy(zoneId1)
         removeGangZone(610)
         addGangZone(610, left, up, right, down, -2130706433)
         zoneActive = true
-        sampAddChatMessage(sName .. 'Территория возвращена! Отсчёт кулдауна запущен!', -1)
+        sampAddChatMessage(sName .. u8:decode('Территория возвращена! Отсчёт кулдауна запущен!'), -1)
 
         timekd = os.date('%X')
         hourkd, minutekd, secundkd = timekd:match('(%d+):(%d+):(%d+)')
-        if minutekd + 30 < 60 then
+        if tonumber(minutekd) + 30 < 60 then
             endkd = hourkd .. ':' .. tonumber(minutekd) + 30 .. ':' .. secundkd
         else
-            endkd = hourkd + 1 .. ':' .. tonumber(minutekd) - 60 + 30 .. ':' .. secundkd
+            endkd = hourkd + 1 .. ':' .. tonumber(minutekd) - 30 .. ':' .. secundkd
         end
         kdcond = true
     end
