acd5afb0ac23337c39117659f5034527814826fa
Tariffka <tariff.uk@gmail.com>
2026-06-04 21:58:33 +0300

    Update script metadata and improve functionality

--- [TJ] trmap.lua
+++ [TJ] trmap.lua
@@ -1,39 +1,39 @@
 script_author('tarif_jan')
-script_name('Êàðòà Êëàäîâ')
+script_name('Карта Кладов')
 script_version('1.0')
 
 local sampev = require 'lib.samp.events'
 
-local sName = '{368b55}[Êàðòà]{FFFFFF} – '
+local sName = '{368b55}[Карта]{FFFFFF} – '
 local zoneActive = false
 local mapUsed = false
 local kdcond = false
-local endkd = 'Íåèçâåñòíî.'
-local autoCopyEnabled = true -- Ôëàã äëÿ àâòîìàòè÷åñêîãî êîïèðîâàíèÿ
+local endkd = 'Неизвестно.'
+local autoCopyEnabled = true -- Флаг для автоматического копирования
 
 function main()
     while not isSampAvailable() do wait(0) end
     wait(100)
-    sampAddChatMessage(sName .. 'Èñïîëüçóéòå êîìàíäû: /zonecopy, /zonepaste, /zonedel, /iskd, /autocopy', -1)
+    sampAddChatMessage(sName .. 'Используйте команды: /zonecopy, /zonepaste, /zonedel, /iskd, /autocopy', -1)
     
-    -- Êîìàíäà äëÿ âêëþ÷åíèÿ/âûêëþ÷åíèÿ àâòî-êîïèðîâàíèÿ
+    -- Команда для включения/выключения авто-копирования
     sampRegisterChatCommand('autocopy', function()
         autoCopyEnabled = not autoCopyEnabled
-        sampAddChatMessage(sName .. (autoCopyEnabled and 'Àâòî-êîïèðîâàíèå âêëþ÷åíî!' or 'Àâòî-êîïèðîâàíèå âûêëþ÷åíî!'), -1)
+        sampAddChatMessage(sName .. (autoCopyEnabled and 'Авто-копирование включено!' or 'Авто-копирование выключено!'), -1)
     end)
     
     sampRegisterChatCommand('zonedel', function()
         removeGangZone(610)
-        sampAddChatMessage(sName .. 'Ïðîøëàÿ òåððèòîðèÿ áûëà óäàëåíà!', -1)
+        sampAddChatMessage(sName .. 'Прошлая территория была удалена!', -1)
     end)
     
     sampRegisterChatCommand('zonecopy', function()
         if mapUsed then
             setClipboardText('/zonepaste l: ' .. left .. '; u: ' .. up .. '; r: ' .. right .. '; d: ' .. down)
-            print('/zonepaste l: ' .. left .. '; u: ' .. up .. '; r: ' .. right .. '; d: ' .. down .. ' - ñêîïèðîâàíî')
-            sampAddChatMessage(sName .. 'Ñêîïèðîâàíî! Îòïðàâü ýòî ÷åëîâåêó, ñ êîòîðûì õî÷åøü ïîäåëèòüñÿ êîîðäèíàòàìè.', -1)
+            print('/zonepaste l: ' .. left .. '; u: ' .. up .. '; r: ' .. right .. '; d: ' .. down .. ' - скопировано')
+            sampAddChatMessage(sName .. 'Скопировано! Отправь это человеку, с которым хочешь поделиться координатами.', -1)
         else
-            sampAddChatMessage(sName .. 'Àêòèâèðóé êàðòó êëàäîâ, ÷òîáû ñêîïèðîâàòü êîîðäèíàòû.', -1)
+            sampAddChatMessage(sName .. 'Активируй карту кладов, чтобы скопировать координаты.', -1)
         end
     end)
     
@@ -46,29 +46,29 @@ function main()
                     removeGangZone(610)
                     addGangZone(610, pLeft, pUp, pRight, pDown, -2130706433)
                     zoneActive = true
-                    sampAddChatMessage(sName .. 'Òåððèòîðèÿ áûëà óñïåøíî äîáàâëåíà íà òâîþ êàðòó!', -1)
+                    sampAddChatMessage(sName .. 'Территория была успешно добавлена на твою карту!', -1)
                 else
-                    sampAddChatMessage(sName .. 'Íå òîò ôîðìàò! Âñòàâü ñþäà êîîðäèíàòû, êîòîðûå òåáå îòïðàâèë äðóã ñ êàðòîé êëàäîâ.', -1)
+                    sampAddChatMessage(sName .. 'Не тот формат! Вставь сюда координаты, которые тебе отправил друг с картой кладов.', -1)
                 end
             else
-                sampAddChatMessage(sName .. 'Òû íè÷åãî íå ââ¸ë! Âñòàâü ñþäà êîîðäèíàòû, êîòîðûå òåáå îòïðàâèë äðóã ñ êàðòîé êëàäîâ.', -1)
+                sampAddChatMessage(sName .. 'Ты ничего не ввёл! Вставь сюда координаты, которые тебе отправил друг с картой кладов.', -1)
             end
         else
             if zoneActive then
                 removeGangZone(610)
                 zoneActive = false
-                sampAddChatMessage(sName .. 'Òåððèòîðèÿ áûëà óäàëåíà. ×òîáû âåðíóòü, ââåäè êîîðäèíàòû åùå ðàç!', -1)
+                sampAddChatMessage(sName .. 'Территория была удалена. Чтобы вернуть, введи координаты еще раз!', -1)
             else
-                sampAddChatMessage(sName .. 'Íå òîò ôîðìàò! Âñòàâü ñþäà êîîðäèíàòû, êîòîðûå òåáå îòïðàâèë äðóã ñ êàðòîé êëàäîâ.', -1)
+                sampAddChatMessage(sName .. 'Не тот формат! Вставь сюда координаты, которые тебе отправил друг с картой кладов.', -1)
             end
         end
     end)
     
     sampRegisterChatCommand('iskd', function(arg)
         if kdcond == true then
-            sampAddChatMessage('{FF0000}[Êàðòà]{FFFFFF} – Ó òåáÿ êóëäàóí. Òû íå ìîæåøü èñïîëüçîâàòü êàðòó! Òâîé êóëäàóí êîí÷èòñÿ ' .. endkd, -1)
+            sampAddChatMessage('{FF0000}[Карта]{FFFFFF} – У тебя кулдаун. Ты не можешь использовать карту! Твой кулдаун кончится ' .. endkd, -1)
         elseif kdcond == false then
-            sampAddChatMessage('{3cb043}[Êàðòà]{FFFFFF} – Êóëäàóíà íåò. Ìîæíî èñïîëüçîâàòü êàðòó!', -1)
+            sampAddChatMessage('{3cb043}[Карта]{FFFFFF} – Кулдауна нет. Можно использовать карту!', -1)
         end
     end)
     
@@ -77,19 +77,19 @@ function main()
         local timenow = os.date('%X')
         if timenow == endkd then
             printStyledString('~r~COOLDOWN END', 5000, 6)
-            sampAddChatMessage('{3cb043}[Êàðòà]{FFFFFF} – Êóëäàóí ïðîøåë! Êàðòó ìîæíî èñïîëüçîâàòü!', -1)
+            sampAddChatMessage('{3cb043}[Карта]{FFFFFF} – Кулдаун прошел! Карту можно использовать!', -1)
             kdcond = false
-            endkd = 'Íåèçâåñòíî.'
+            endkd = 'Неизвестно.'
         end
     end
 end
 
 
 function sampev.onServerMessage(_, text)
-    if autoCopyEnabled and text:find('Òåððèòîðèÿ íàéäåíà! Ïîñëå åå èñ÷åçíîâåíèÿ îíà áóäåò àâòîìàòè÷åñêè âîññòàíîâëåíà') then
+    if autoCopyEnabled and text:find('Территория найдена! После ее исчезновения она будет автоматически восстановлена') then
         lua_thread.create(function()
             wait(200)
-			sampAddChatMessage(sName .. 'Òåêñò áûë îáíàðóæåí, ïðîèçâîäèòñÿ àâòîìàòè÷åñêèé /zonecopy', -1)
+			sampAddChatMessage(sName .. 'Текст был обнаружен, производится автоматический /zonecopy', -1)
 			wait(400)
 			sampProcessChatInput("/zonecopy")
         end)
@@ -106,8 +106,8 @@ function sampev.onCreateGangZone(zoneId, squareStart, squareEnd, color)
         right = squareEnd.x
         down = squareEnd.y
         print('l: ' .. left .. '; u: ' .. up .. '; r: ' .. right .. '; d: ' .. down)
-        sampAddChatMessage(sName .. 'Òåððèòîðèÿ íàéäåíà! Ïîñëå åå èñ÷åçíîâåíèÿ îíà áóäåò àâòîìàòè÷åñêè âîññòàíîâëåíà.', -1)
-        sampAddChatMessage(sName .. '×òîáû ñêîïèðîâàòü åå êîîðäèíàòû, ïðîïèøè /zonecopy', -1)
+        sampAddChatMessage(sName .. 'Территория найдена! После ее исчезновения она будет автоматически восстановлена.', -1)
+        sampAddChatMessage(sName .. 'Чтобы скопировать ее координаты, пропиши /zonecopy', -1)
     end
 end
 
@@ -116,7 +116,7 @@ function sampev.onGangZoneDestroy(zoneId1)
         removeGangZone(610)
         addGangZone(610, left, up, right, down, -2130706433)
         zoneActive = true
-        sampAddChatMessage(sName .. 'Òåððèòîðèÿ âîçâðàùåíà! Îòñ÷¸ò êóëäàóíà çàïóùåí!', -1)
+        sampAddChatMessage(sName .. 'Территория возвращена! Отсчёт кулдауна запущен!', -1)
 
         timekd = os.date('%X')
         hourkd, minutekd, secundkd = timekd:match('(%d+):(%d+):(%d+)')
